e120058ecd7a93d5dea56bf1270d544ad658797c
Paweł Stiasny <pawelstiasny@gmail.com>
2024-12-05 14:31:00 +0100

    day 5

---
+++ d5/p1.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+
+from collections import defaultdict
+import fileinput
+import networkx as nx
+
+
+inp = fileinput.input(encoding='utf-8')
+before = defaultdict(list)
+
+# g = nx.DiGraph()
+
+for l in inp:
+    l = l.strip()
+    if l == '':
+        break
+    left, right = l.split('|')
+    before[int(right)].append(int(left))
+    # g.add_edge(left, right)
+
+
+mids = []
+for l in inp:
+    q = [int(j) for j in l.strip().split(',')]
+    al = set(q)
+    got = set()
+    good = True
+    for i in q:
+        for j in before[i]:
+            if j in al and not j in got:
+                good = False
+
+        got.add(i)
+
+    if good:
+        print(q)
+        mids.append(q[len(q)//2])
+
+print(sum(mids))
